5e2737c2c86e572c1e68789ff41780e2791e5456
ahyoon <kimay9623@naver.com>
2021-01-28 01:06:24 +0900

    initial code

---
+++ hello-spring/src/test/java/Ahyoon/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Ahyoon.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
